610df55a510522848558f9f0f115a831616f8ad8
Tim Nolte <tim.nolte@ndigitals.com>
2022-12-30 05:49:10 +0000

    fix(admin_php): Fixes PHP 7 requirement for the Web Console

--- template/config-ols-admin_php.sh
+++ template/config-ols-admin_php.sh
@@ -5,6 +5,6 @@
 cd $LSWS_HOME/admin/fcgi-bin || exit
 
 # Copy admin_php to destination and setup for execution.
-cp $LSWS_HOME/phpbuild/php-$PHP_VERSION/sapi/litespeed/php admin_php-${PHP_MAJOR_VERSION}
-chmod a+rx admin_php-${PHP_MAJOR_VERSION}
-ln -sf admin_php-${PHP_MAJOR_VERSION} admin_php
+cp $LSWS_HOME/phpbuild/php-$OLS_ADMIN_PHP_VERSION/sapi/litespeed/php admin_php-${OLS_ADMIN_PHP_MAJOR_VERSION}
+chmod a+rx admin_php-${OLS_ADMIN_PHP_MAJOR_VERSION}
+ln -sf admin_php-${OLS_ADMIN_PHP_MAJOR_VERSION} admin_php
