763feff44bf3561367c27efddf028e928b236649
Tim Nolte <tim.nolte@ndigitals.com>
2023-01-05 19:17:41 -0500

    fix(lsphp): Fixes invalid runtime lsphp binary and symlinks
    * Creates proper symlinks for teh fcgi lsphp after bad OLS compile bad binary is created.

--- template/config-php.sh
+++ template/config-php.sh
@@ -8,8 +8,6 @@ ln -sf $LSWS_HOME/lsphp${PHP_MAJOR_VERSION}${PHP_MINOR_VERSION}/bin/php${PHP_MAJ
 ln -sf $LSWS_HOME/lsphp${PHP_MAJOR_VERSION}${PHP_MINOR_VERSION}/bin/php${PHP_MAJOR_VERSION}.${PHP_MINOR_VERSION} $LSWS_HOME/lsphp${PHP_MAJOR_VERSION}${PHP_MINOR_VERSION}/bin/php
 ln -sf $LSWS_HOME/lsphp${PHP_MAJOR_VERSION}${PHP_MINOR_VERSION}/bin/lsphp${PHP_MAJOR_VERSION}.${PHP_MINOR_VERSION} /usr/local/bin/lsphp
 ln -sf $LSWS_HOME/lsphp${PHP_MAJOR_VERSION}${PHP_MINOR_VERSION}/bin/lsphp${PHP_MAJOR_VERSION}.${PHP_MINOR_VERSION} $LSWS_HOME/lsphp${PHP_MAJOR_VERSION}${PHP_MINOR_VERSION}/bin/lsphp
-ln -sf $LSWS_HOME/lsphp${PHP_MAJOR_VERSION}${PHP_MINOR_VERSION}/bin/lsphp $LSWS_HOME/fcgi-bin/lsphp8
-ln -sf $LSWS_HOME/fcgi-bin/lsphp8 $LSWS_HOME/fcgi-bin/lsphp
 
 # Install missing PECL PHP modules.
 $LSWS_HOME/lsphp${PHP_MAJOR_VERSION}${PHP_MINOR_VERSION}/bin/pecl install igbinary
