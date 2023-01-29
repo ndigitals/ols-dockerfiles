d5b82e345a7d856d4b03adf28995a188f93dec3a
Tim Nolte <tim.nolte@ndigitals.com>
2023-01-28 22:25:29 -0500

    feat(ols): Adds additional module & server language(Node & Ruby) support. (#55)
    * Adds ModSecurity module build to Docker image build process.
    * Adds support for NodeJS 16.
    * Adds support for Ruby.

--- template/prepare-ols.sh
+++ template/prepare-ols.sh
@@ -4,7 +4,10 @@
 
 cd /build || exit 
 
-# Build OpenLiteSpeed from source.
+# carefully, it's import to export PKG_CONFIG_PATH to tell the pkg-config the info about yajl
+export PKG_CONFIG_PATH=/usr/local/share/pkgconfig:PKG_CONFIG_PATH
+
+# Download and prepare OpenLiteSpeed source.
 wget https://openlitespeed.org/packages/openlitespeed-$OLS_VERSION.src.tgz && \
-	tar xzf openlitespeed-$OLS_VERSION.src.tgz && cd openlitespeed-$OLS_VERSION
+	tar xzf openlitespeed-$OLS_VERSION.src.tgz
 
