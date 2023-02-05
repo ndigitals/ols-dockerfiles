347011a89842c70aece0d3b440ea6496d7338f5e
Tim Nolte <tim.nolte@ndigitals.com>
2023-02-05 18:38:31 +0000

    fix(php:extensions): Adds missing PHP modules/extensions
    * Adds missing Ctype extension.
    * Adds XMLRPC PECL extension.

--- template/config-php.sh
+++ template/config-php.sh
@@ -15,5 +15,6 @@ $LSWS_HOME/lsphp${PHP_MAJOR_VERSION}${PHP_MINOR_VERSION}/bin/pecl install msgpac
 $LSWS_HOME/lsphp${PHP_MAJOR_VERSION}${PHP_MINOR_VERSION}/bin/pecl install lzf
 $LSWS_HOME/lsphp${PHP_MAJOR_VERSION}${PHP_MINOR_VERSION}/bin/pecl install zstd
 $LSWS_HOME/lsphp${PHP_MAJOR_VERSION}${PHP_MINOR_VERSION}/bin/pecl install imagick
+$LSWS_HOME/lsphp${PHP_MAJOR_VERSION}${PHP_MINOR_VERSION}/bin/pecl install xmlrpc
 yes "yes" | $LSWS_HOME/lsphp${PHP_MAJOR_VERSION}${PHP_MINOR_VERSION}/bin/pecl install redis
 printf "\n\n\nyes\nyes\nyes\n\nyes\nyes\n" | $LSWS_HOME/lsphp${PHP_MAJOR_VERSION}${PHP_MINOR_VERSION}/bin/pecl install memcached
